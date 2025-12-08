c425207e613b1da047a9d25224aa83f071d79c7d
Ekshitha Promantia <ekshitha.avilala@promantia.com>
2025-12-08 18:10:10 +0530

    Initial commit - gym_Management

---
+++ gym_management/gym_management/doctype/gym_membership_card/gym_membership_card.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ekshitha and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Gym Membership Card", {
+// 	refresh(frm) {
+
+// 	},
+// });
