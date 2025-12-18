73bd744a008cc35ae6ef012ebb08fb86276a5137
Ekshitha Promantia <ekshitha.avilala@promantia.com>
2025-12-18 11:10:29 +0530

    Gym members and workout plan development

--- gym_management/gym_management/doctype/gym_workout_plan/gym_workout_plan.py
+++ gym_management/gym_management/doctype/gym_workout_plan/gym_workout_plan.py
@@ -5,5 +5,21 @@
 from frappe.model.document import Document
 
 
+<<<<<<< Updated upstream
 class GymWorkoutPlan(Document):
 	pass
+=======
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class GymWorkoutPlan(WebsiteGenerator):
+    pass
+
+    # def get_context(self, context):
+    #     context.no_cache = 1
+    #     return context
+
+  
+ 
+>>>>>>> Stashed changes
