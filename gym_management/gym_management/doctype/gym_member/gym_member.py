503d0a41ae92854b27d78cd8aa09a35a861b3025
Ekshitha Promantia <ekshitha.avilala@promantia.com>
2025-12-18 11:33:51 +0530

    Revert "Gym members and workout plan development"
    This reverts commit 73bd744a008cc35ae6ef012ebb08fb86276a5137.

--- gym_management/gym_management/doctype/gym_member/gym_member.py
+++ gym_management/gym_management/doctype/gym_member/gym_member.py
@@ -1,13 +1,5 @@
 # Copyright (c) 2025, ekshitha and contributors
 # For license information, please see license.txt
-<<<<<<< Updated upstream
-=======
-#import frappe
-#from frappe.website.website_generator import WebsiteGenerator
-import frappe
-from frappe.website.website_generator import WebsiteGenerator
-from frappe.utils import validate_email_address
->>>>>>> Stashed changes
 
 # import frappe
 from frappe.model.document import Document
--- gym_management/gym_management/doctype/gym_workout_plan/gym_workout_plan.py
+++ gym_management/gym_management/doctype/gym_workout_plan/gym_workout_plan.py
@@ -5,21 +5,5 @@
 from frappe.model.document import Document
 
 
-<<<<<<< Updated upstream
 class GymWorkoutPlan(Document):
 	pass
-=======
-import frappe
-from frappe.website.website_generator import WebsiteGenerator
-
-
-class GymWorkoutPlan(WebsiteGenerator):
-    pass
-
-    # def get_context(self, context):
-    #     context.no_cache = 1
-    #     return context
-
-  
- 
->>>>>>> Stashed changes
