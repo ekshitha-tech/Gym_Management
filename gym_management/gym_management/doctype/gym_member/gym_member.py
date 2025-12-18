6e98993add82ac1b02a57c363758370422249753
Ekshitha Promantia <ekshitha.avilala@promantia.com>
2025-12-18 12:52:49 +0530

    changes

--- gym_management/gym_management/doctype/gym_member/gym_member.py
+++ gym_management/gym_management/doctype/gym_member/gym_member.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2025, ekshitha and contributors
 # For license information, please see license.txt
-#import frappe
-#from frappe.model.document import Document
+
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
 from frappe.utils import validate_email_address
